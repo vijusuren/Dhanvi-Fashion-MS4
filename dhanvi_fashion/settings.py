3899aa0030440099016cdfb8bd7a4624d0c13d5d
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-04 07:14:55 +0000

    fix nav bar bug adding style for cover text add product

--- dhanvi_fashion/settings.py
+++ dhanvi_fashion/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 
 ]
 
@@ -69,9 +70,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', #required by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
