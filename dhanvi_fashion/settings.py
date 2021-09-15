131c2768b25521ab7d90a731f574fe4f112f9ee7
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-15 17:30:42 +0000

    added checkout views, models and templates

--- dhanvi_fashion/settings.py
+++ dhanvi_fashion/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dhanvi_fashion.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,6 +82,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]            
         },
     },
 ]
