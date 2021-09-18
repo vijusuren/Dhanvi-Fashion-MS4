1e2c7dfd6e20f65087651a5756c17e21f9959731
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-18 15:51:21 +0000

    completed checkout with error TypeError at checkout

--- dhanvi_fashion/settings.py
+++ dhanvi_fashion/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-
+    
     # Other
     'crispy_forms',
 ]
@@ -175,6 +175,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
