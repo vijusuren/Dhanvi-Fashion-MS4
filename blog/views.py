ca0982de30c6122c94a4b3b4826e8adc0e40e262
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-21 14:52:18 +0000

    add templates, css and view for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import BlogPost
 
-# Create your views here.
+def all_blog_posts(request):
+    """
+    A view to return all the blog posts
+    """
+    blogs = BlogPost.objects.all()
+    context = {
+        'blogs': blogs,
+    }
+
+    return render(request, 'blog/blog_posts.html', context)
