cf488bcd6327ab32c70352aaebc5a860c09684a1
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-21 23:10:41 +0000

    add migration

--- blog/models.py
+++ blog/models.py
@@ -29,7 +29,8 @@ class BlogPost(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(BlogPost,
+                             on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
