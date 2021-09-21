5df84b94f5d90d7427bc22dc8beb057d7d250975
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-21 14:42:02 +0000

    add admin to the blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    """
+    Admin display for blog post.
+    """
+    list_display = (
+        'title',
+        'author',
+        'created_on',
+        'status',
+    )
+    list_filter = ("status",)
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
