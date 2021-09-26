b8385063ff9b6cacb35ba2b093b1e24b9610570a
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-26 23:19:35 +0000

    delete default primary key field

--- dhanvi_fashion/settings.py
+++ dhanvi_fashion/settings.py
@@ -213,8 +213,3 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'dhanvifashion@example.com'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
