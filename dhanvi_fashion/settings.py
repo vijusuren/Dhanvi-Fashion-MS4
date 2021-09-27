c52f31048f0494888cd5317db97e96604e691ef0
vijusuren <vijayalakshmioviya@gmail.com>
2021-09-27 16:40:36 +0000

    add if condition in setting

--- dhanvi_fashion/settings.py
+++ dhanvi_fashion/settings.py
@@ -125,16 +125,22 @@ WSGI_APPLICATION = 'dhanvi_fashion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = {
-    'default' : dj_database_url.parse('postgres://phquvzlqogxzxf:bffc9fb78810c0050ea2468e1853a7176b5c78c0c4d6c231112265a7b26252d1@ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d69o863huiraa2')
-}
+
+#DATABASES = {
+#    'default' : dj_database_url.parse('postgres://phquvzlqogxzxf:bffc9fb78810c0050ea2468e1853a7176b5c78c0c4d6c231112265a7b26252d1@ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d69o863huiraa2')
+#}
 
 
 # Password validation
